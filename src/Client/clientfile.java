5a2615b1236055770eaee86b25e0d1510d33a71b
Shivani Kadam <60286618+shivanikadam910@users.noreply.github.com>
2020-11-07 14:01:17 +0530

    Add files via upload

--- src/Client/clientfile.java
+++ src/Client/clientfile.java
@@ -8,37 +8,66 @@
  *
  * @author Shivani Kadam
  */
-import Server.FileEvent;
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class clientfile {
 
-    private static String sourceFilePath;
-    private static ObjectInputStream inputStream =null;
+    private static String sourceFilePath1;
+    private static String sourceFilePath2;
+    private static ObjectInputStream inputStream = null;
+    private static ObjectOutputStream outputStream = null;
     private FileEvent fileEvent = null;
     private File dstFile = null;
+    private static String fname = null;
     private FileOutputStream fileOutputStream = null;
+    private String destinationPath = "F:/study/sem5/";
 
     public static void main(String args[]) throws Exception {
         Socket sock = new Socket("127.0.0.1", 1234);
-
-        // reading the file name 
-        System.out.print("Enter the file name : ");
-        BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
-        sourceFilePath = keyRead.readLine();
-
-        // sending the file name to server
-        OutputStream ostream = sock.getOutputStream();
-        PrintWriter pwrite = new PrintWriter(ostream, true);
-        pwrite.println(sourceFilePath);
-        inputStream = new ObjectInputStream(sock.getInputStream());
         clientfile o1 = new clientfile();
-        o1.downloadFile();
+        Scanner input = new Scanner(System.in);
+
         
-        sock.close();
-        ostream.close();;
-        pwrite.close();
+            System.out.println("Want to upload or download ? ");
+            String opt = input.next();
+            if (opt.equals("upload")) {
+                System.out.print("Enter the file name : ");
+                BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+              //  sourceFilePath1 = keyRead.readLine();
+                
+                //InputStream istream = sock.getInputStream();
+                //BufferedReader fileRead = new BufferedReader(new InputStreamReader(istream));
+                fname = keyRead.readLine();
+                
+                outputStream = new ObjectOutputStream(sock.getOutputStream());
+                System.out.println("send thvi joie");
+                o1.sendFile();
+                sock.close();
+                keyRead.close();
+                
+
+            } else if (opt.equals("download")) {
+                System.out.print("Enter the file name : ");
+                BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+                sourceFilePath2 = keyRead.readLine();
+
+                // sending the file name to server
+                OutputStream ostream = null;
+                ostream = sock.getOutputStream();
+                PrintWriter pwrite = new PrintWriter(ostream, true);
+                pwrite.println(sourceFilePath2);
+                inputStream = new ObjectInputStream(sock.getInputStream());
+                System.out.println("download nai");
+                o1.downloadFile();
+                sock.close();
+                ostream.close();
+                pwrite.close();
+            } else {
+                System.out.println("please type upload or download.");
+            }
+        
     }
 
     public void downloadFile() {
@@ -68,5 +97,49 @@ public class clientfile {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+    }
+
+    public void sendFile() {
+        fileEvent = new FileEvent();
+        String fileName = fname.substring(fname.lastIndexOf("/") + 1, fname.length());
+        String path = fname.substring(0, fname.lastIndexOf("/") + 1);
+        fileEvent.setDestinationDirectory(destinationPath);
+        fileEvent.setFilename(fileName);
+        fileEvent.setSourceDirectory(fname);
+        File file = new File(fname);
+        if (file.isFile()) {
+            try {
+                DataInputStream diStream = new DataInputStream(new FileInputStream(file));
+                long len = (int) file.length();
+                byte[] fileBytes = new byte[(int) len];
+                int read = 0;
+                int numRead = 0;
+                while (read < fileBytes.length && (numRead = diStream.read(fileBytes, read, fileBytes.length - read)) >= 0) {
+                    read = read + numRead;
+                }
+                fileEvent.setFileSize(len);
+                fileEvent.setFileData(fileBytes);
+                fileEvent.setStatus("Success");
+            } catch (Exception e) {
+                e.printStackTrace();
+                fileEvent.setStatus("Error");
+            }
+        } else {
+            System.out.println("path specified is not pointing to a file");
+            fileEvent.setStatus("Error");
+        }
+//Now writing the FileEvent object to socket
+        try {
+            outputStream.writeObject(fileEvent);
+            System.out.println("Done...Going to exit");
+            Thread.sleep(3000);
+            System.exit(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
